d051d0ccadfad86985b446dd5c7b12f6577df096
차요셉 <jscha@raoncorp.com>
2022-02-16 22:06:18 +0900

    네비게이션바, 자산 메뉴 그리드뷰, 페이징 배너 추가 (자산 탭)

---
+++ MyAssets/MyAssets/AssetView.swift
@@ -0,0 +1,31 @@
+//
+//  AssetView.swift
+//  MyAssets
+//
+//  Created by Joseph Cha on 2022/02/16.
+//
+
+import SwiftUI
+
+struct AssetView: View {
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                VStack(spacing: 30) {
+                    Spacer()
+                    AssetMenuGridView()
+                    AssetBannerView()
+                        .aspectRatio(5/2, contentMode: .fit)
+                }
+            }
+            .background(Color.gray.opacity(0.2))
+            .navigationBarWithButtonStyle("내 자산")
+        }
+    }
+}
+
+struct AssetView_Previews: PreviewProvider {
+    static var previews: some View {
+        AssetView()
+    }
+}
---
+++ MyAssets/MyAssets/Entities/AssetBanner.swift
@@ -0,0 +1,15 @@
+//
+//  AssetBanner.swift
+//  MyAssets
+//
+//  Created by Joseph Cha on 2022/02/16.
+//
+
+import Foundation
+import UIKit
+
+struct AssetBanner {
+    let title: String
+    let description: String
+    let backgroundColor: UIColor
+}
